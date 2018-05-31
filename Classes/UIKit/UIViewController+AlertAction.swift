cf2a66d29e8626b9f184fd8d44d9542086c93809
Carsten Schlobohm <carsten.schlobohm@apploft.de>
2018-05-31 16:13:30 +0200

    update AlertAction extension

--- Classes/UIKit/UIViewController+AlertAction.swift
+++ Classes/UIKit/UIViewController+AlertAction.swift
@@ -6,34 +6,34 @@
 
 import UIKit
 
-struct AlertButton {
-    let title: String
-    let enabled: Bool
-    let action: () -> Void
-    var style: UIAlertActionStyle
+public struct AlertButton {
+    public let title: String
+    public let enabled: Bool
+    public let action: () -> Void
+    public var style: UIAlertActionStyle
     
-    init(title: String, enabled: Bool = true, action: @escaping () -> Void = {}) {
+    public init(title: String, enabled: Bool = true, action: @escaping () -> Void = {}) {
         self.init(title: title, enabled: enabled, style: .default, action: action)
     }
     
-    init(title: String, enabled: Bool = true, style: UIAlertActionStyle = .default, action: @escaping () -> Void = {}) {
+    public init(title: String, enabled: Bool = true, style: UIAlertActionStyle = .default, action: @escaping () -> Void = {}) {
         self.title = title
         self.action = action
         self.enabled = enabled
         self.style = style
     }
     
-    static func okButton(enabled: Bool = true, action: @escaping () -> Void = {}) -> AlertButton {
+    public static func okButton(enabled: Bool = true, action: @escaping () -> Void = {}) -> AlertButton {
         return AlertButton(title: NSLocalizedString("ok", comment: ""), enabled: enabled, action: action)
     }
     
-    static func cancelButton(enabled: Bool = true, action: @escaping () -> Void = {}) -> AlertButton {
+    public static func cancelButton(enabled: Bool = true, action: @escaping () -> Void = {}) -> AlertButton {
         return AlertButton(title: NSLocalizedString("cancel", comment: ""), enabled: enabled, action: action)
     }
 }
 
-extension UIViewController {
-    func alert(title: String?, message: String?, cancelButton: AlertButton, otherButtons: [AlertButton]) {
+public extension UIViewController {
+    public func alert(title: String?, message: String?, cancelButton: AlertButton, otherButtons: [AlertButton]) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let cancelAction = UIAlertAction(title: cancelButton.title, style: .cancel) { (action: UIAlertAction!) -> Void in
@@ -50,25 +50,25 @@ extension UIViewController {
         present(alertController, animated: true, completion: nil)
     }
     
-    func alertOk(title: String?, message: String?, completionHandler: @escaping () -> Void) {
+    public func alertOk(title: String?, message: String?, completionHandler: @escaping () -> Void) {
         let okButton = AlertButton(title: NSLocalizedString("ok", comment: ""), action: completionHandler)
         
         alert(title: nil, message: message, cancelButton: okButton, otherButtons: [])
     }
     
-    func alertError(title: String?, message: String?, completionHandler: @escaping () -> Void = {}) {
+    public func alertError(title: String?, message: String?, completionHandler: @escaping () -> Void = {}) {
         let okButton = AlertButton(title: NSLocalizedString("ok", comment: ""), action: completionHandler )
         
         alert(title: title, message: message, cancelButton: okButton, otherButtons: [])
     }
     
-    func actionSheet(_ title: String? = nil, cancelButton: AlertButton? = nil, destructiveButton: AlertButton? = nil, otherButtons: [AlertButton] = [], _ portraitOnly: Bool = false) {
+    public func actionSheet(_ title: String? = nil, cancelButton: AlertButton? = nil, destructiveButton: AlertButton? = nil, otherButtons: [AlertButton] = [], _ portraitOnly: Bool = false) {
         let destructiveButtons = destructiveButton.map { [$0] } ?? []
         
         actionSheet(title, cancelButton: cancelButton, destructiveButtons: destructiveButtons, otherButtons: otherButtons, portraitOnly)
     }
     
-    func actionSheet(_ title: String? = nil, cancelButton: AlertButton? = nil, destructiveButtons: [AlertButton] = [], otherButtons: [AlertButton] = [], _ portraitOnly: Bool = false) {
+    public func actionSheet(_ title: String? = nil, cancelButton: AlertButton? = nil, destructiveButtons: [AlertButton] = [], otherButtons: [AlertButton] = [], _ portraitOnly: Bool = false) {
         let alertController = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
         
         if let cancelButton = cancelButton {
