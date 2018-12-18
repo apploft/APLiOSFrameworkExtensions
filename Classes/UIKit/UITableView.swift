2d80076e8f29125b260e2ac5da546c25ab79efda
Tino Rachui <tino.rachui@apploft.de>
2018-12-18 15:15:55 +0100

    Added documentation
    Added missing documentation

--- Classes/UIKit/UITableView.swift
+++ Classes/UIKit/UITableView.swift
@@ -4,6 +4,11 @@
 import UIKit
 
 extension UITableView {
+    
+    /// Given a view determine the index path of the corresponding
+    /// table view cell.
+    /// - Parameter view: the view contained in any of the cells
+    /// - Returns: the corresponding index path or nil
     func indexPathForView(_ view: UIView) -> IndexPath? {
         let point = view.convert(CGPoint.zero, to: self)
         return indexPathForRow(at: point)
