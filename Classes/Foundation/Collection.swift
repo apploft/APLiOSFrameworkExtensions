3e9d1111b016fc46d0324409253d1e7e3c1fd255
philipkrck <46320394+philipkrck@users.noreply.github.com>
2019-01-03 16:27:18 +0100

    example add under discussion subsection

--- Classes/Foundation/Collection.swift
+++ Classes/Foundation/Collection.swift
@@ -9,6 +9,14 @@ public extension Collection {
     /// Safely access collection elements.
     ///
     /// - Parameter index: the index of the element
+    ///
+    /**
+     *Example*
+     
+     let array = ["a", "b"]
+     array[2] // crashes
+     array(safe: 2) // nil
+     */
     public subscript (safe index: Index) -> Element? {
         guard indices.contains(index) else { return nil }
         
