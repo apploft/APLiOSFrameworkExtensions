60446029c203311b557a309a3053b401610da865
Philip Krueck <philip.krueck@apploft.de>
2019-01-09 09:37:59 +0100

    DispatchQuestTests: tests written

--- Sample/SampleTests/DispatchQueueTest.swift
+++ Sample/SampleTests/DispatchQueueTest.swift
@@ -13,11 +13,24 @@ class DispatchQueueTest: XCTestCase {
     func testOnceQueue() {
         var number = 0
         for _ in 0...3 {
-            DispatchQueue.once(token: "token") {
+            DispatchQueue.once(token: "com.apploft.token") {
                 number += 5
             }
         }
         XCTAssertEqual(number, 5)
     }
-
+    
+    func testOnceQueueDiffBlock() {
+        var number = 0
+        var blockOne: () -> Void = {
+            number += 5
+        }
+        var blockTwo: () -> Void = {
+            number += 100
+        }
+        DispatchQueue.once(token: "com.apploft.token", block: blockOne)
+        DispatchQueue.once(token: "com.apploft.token", block: blockTwo)
+        
+        XCTAssertEqual(number, 5)
+    }
 }
