75add111732d3f442e7103f4f968197abc423544
Philip Krueck <philip.krueck@apploft.de>
2019-01-09 17:44:07 +0100

    created Error2 to see if  APLError can be improved using an enum

---
+++ Classes/Foundation/Error2.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorOptimized?.swift
+//  APLiOSFrameworkExtensions
+//
+//  Created by Philip Krück on 09.01.19.
+//
+
+// maybe this makes more sense than Error.swift? idk
+
+import Foundation
+
+enum APLError2: Error {
+    
+}
+
