7557037aefd2d9c951d547fda77c48514d2c5506
Philip Krueck <philip.krueck@apploft.de>
2019-01-09 17:58:58 +0100

    used enum to represent APLError types: leads to code reduction and more elegant way to use error protocol (according to Apple Doc). Possible downsides to this approach should be further investigated.

--- Classes/Foundation/Error2.swift
+++ Classes/Foundation/Error2.swift
@@ -9,7 +9,34 @@
 
 import Foundation
 
-enum APLError2: Error {
+enum APLError2: Int, Error {
+    public static var errorDomain: String {
+        return "de.apploft"
+    }
     
+    case fileNotFound = 1000
 }
 
+
+public extension NSError {
+    
+    /// custom fileNotFound error
+    public static var fileNotFound2: NSError {
+        return NSError(domain: APLError2.errorDomain, code: APLError2.fileNotFound.rawValue, userInfo: nil)
+    }
+}
+
+
+public extension Error {
+    /// Assuming this is an instance of NSError return the 'code'
+    public var code2: Int {
+        return (self as NSError).code
+    }
+    
+    /// Assuming this is an instance of NSError return the 'domain'
+    public var domain2: String {
+        return (self as NSError).domain
+    }
+    
+    
+}
