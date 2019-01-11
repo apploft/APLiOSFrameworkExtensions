8a640b8de30bd8ceb350588c9fa75d48d22f9644
Philip Krueck <philip.krueck@apploft.de>
2019-01-11 12:02:49 +0100

    added documentation to Optional extension for different methods || created OptionalTest unit testing file including methods that will need further implementation

--- Classes/Foundation/Optional.swift
+++ Classes/Foundation/Optional.swift
@@ -31,6 +31,9 @@ extension Optional {
     ///
     /// - Parameter defaultValue: value for wrapped optional if it isn't nil
     /// - Returns: returns value if not nil, otherwise returns value of the passed in parameter
+    ///
+    /// The method has the same functionality as the '??' nil-coalescing operator bus as a function.
+    /// Use this when code is unreadable due to use of many operators
     func value(or defaultValue: Wrapped) -> Wrapped {
         return self ?? defaultValue
     }
@@ -60,15 +63,15 @@ extension EmptyValueRepresentable {
     }
 }
 
+extension String: EmptyValueRepresentable {
+    /// representation for an empty value for a String
+    public static var emptyValue: String { return "" }
+}
+
 extension Array: EmptyValueRepresentable {
     
     /// representation for an empty value for an Array
     public static var emptyValue: [Element] { return [] }
-}
-
-extension Set: EmptyValueRepresentable {
-    /// representation for an empty value for a Set
-    public static var emptyValue: Set<Element> { return Set() }
 }
 
 extension Dictionary: EmptyValueRepresentable {
@@ -76,10 +79,12 @@ extension Dictionary: EmptyValueRepresentable {
     public static var emptyValue: Dictionary<Key, Value> { return [:] }
 }
 
-extension String: EmptyValueRepresentable {
-    /// representation for an empty value for a String
-    public static var emptyValue: String { return "" }
+extension Set: EmptyValueRepresentable {
+    /// representation for an empty value for a Set
+    public static var emptyValue: Set<Element> { return Set() }
 }
+
+
 
 public extension Optional where Wrapped: EmptyValueRepresentable {
 
---
+++ Sample/SampleTests/OptionalTest.swift
@@ -0,0 +1,91 @@
+//
+//  OptionalTest.swift
+//  SampleTests
+//
+//  Created by Philip Krück on 11.01.19.
+//  Copyright © 2019 Tino Rachui. All rights reserved.
+//
+
+import XCTest
+
+class OptionalTest: XCTestCase {
+    
+    // MARK: test 'isNilOrEmpty'
+
+    func testIsNilOrEmptyWhenStringIsEmpty() {
+//        let emptyString: String? = ""
+//        XCTAssertTrue(emptyString.isNilOrEmpty)
+    }
+    
+    func testIsNilOrEmptyWhenStringIsNil() {
+        
+    }
+    
+    func testIsNilOrEmptyWhenArrayIsEmpty() {
+        
+    }
+
+    func testIsNilOrEmptyWhenArrayIsNil() {
+        
+    }
+    
+    func testIsNilOrEmptyWhenDictionaryIsEmpty() {
+        
+    }
+    
+    func testIsNilOrEmptyWhenDictionaryIsNil() {
+        
+    }
+    
+    func testIsNilOrEmptyWhenSetIsEmpty() {
+        
+    }
+    
+    func testIsNilOrEmptyWhenSetIsNil() {
+        
+    }
+    
+    //: MARK: test 'valueOrNil'
+    func testValueOrNilWhenVariableIsNil() {
+//        let someInt: Int? = 3
+//        XCTAssertNotNil(someInt.value(or:))
+    }
+    
+    func testValueOrNilWhenVariableHasValue() {
+//        let someInt: Int? = nil
+//        XCTAssertNil(someInt.value(or:))
+    }
+    
+    //: MARK: test 'nilIfEmpty'
+    func testNilIfEmptyWhenStringIsNil() {
+        
+    }
+    
+    func testNilIfEmptyWhenStringIsEmpty() {
+        
+    }
+    
+    func testNilIfEmptyWhenArrayIsNil() {
+        
+    }
+    
+    func testNilIfEmptyWhenArrayIsEmpty() {
+        
+    }
+    
+    func testNilIfEmptyWhenDictionaryIsNil() {
+        
+    }
+    
+    func testNilIfEmptyWhenDictionaryIsEmpty() {
+        
+    }
+    
+    func testNilIfEmptyWhenSetIsNil() {
+        
+    }
+    
+    func testNilIfEmptyWhenSetIsEmpty() {
+        
+    }
+}
