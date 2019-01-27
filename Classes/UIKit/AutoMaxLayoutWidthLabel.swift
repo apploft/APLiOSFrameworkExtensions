6067f1445a2459cffd99d2df79ec5159dc94a422
Tino Rachui <tino.rachui@apploft.de>
2019-01-27 15:54:06 +0100

    Added a label subclass which automatically sets its 'preferredMaxLayoutWidth' based on its current frame width

---
+++ Classes/UIKit/AutoMaxLayoutWidthLabel.swift
@@ -0,0 +1,42 @@
+//
+// Created by apploft on 27.01.2019.
+// Copyright © 2019 apploft GmbH.
+// MIT License · http://choosealicense.com/licenses/mit/
+
+import UIKit
+
+public class AutoMaxLayoutWidthLabel: UILabel {
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    func commonInit() {
+        setContentCompressionResistancePriority(UILayoutPriority(rawValue: 1000), for: .horizontal)
+        setContentCompressionResistancePriority(UILayoutPriority(rawValue: 1000), for: .vertical)
+
+        setContentHuggingPriority(UILayoutPriority(rawValue: 1000), for: .horizontal)
+        setContentHuggingPriority(UILayoutPriority(rawValue: 1000), for: .vertical)
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if preferredMaxLayoutWidth != frame.size.width {
+            preferredMaxLayoutWidth = frame.size.width
+            setNeedsUpdateConstraints()
+        }
+    }
+
+    public override var intrinsicContentSize: CGSize {
+        var size = super.intrinsicContentSize
+        size.height += 1.0
+        return size
+    }
+}
