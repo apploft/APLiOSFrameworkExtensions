3ba76ba949b39ddcc5d057414448a4f666f83b18
Philip Krueck <philip.krueck@apploft.de>
2019-02-07 09:43:30 +0100

    added objective C mapping to UIViewController extension

--- Classes/UIKit/UIViewController.swift
+++ Classes/UIKit/UIViewController.swift
@@ -5,14 +5,14 @@
 
 import UIKit
 
-public extension UIViewController {
+@objc public extension UIViewController {
     
     /// Determine the top visible view controller starting from the given view
     /// controller, considering presented view controller recursively.
     /// - Parameter controller: the staring view controller. Default will be the
     /// 'rootViewController'
     /// - Returns: the top visible view controller or nil
-    public class func topViewController(of controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+    @objc public class func topViewController(of controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
         if let presented = controller?.presentedViewController {
             return topViewController(of: presented)
         }
